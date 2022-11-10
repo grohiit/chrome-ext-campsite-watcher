813b3e1050af9cccbd92f5aa98defc4339ec3878
grohiit <81778041+grohiit@users.noreply.github.com>
2022-11-10 15:37:08 -0500

    basic version

--- script.js
+++ script.js
@@ -1,10 +1,62 @@
 
-const urlPattern = /https:\/\/reservecalifornia.com\/Web\/#!park\/.+\/.+/;
-const urlMatched = urlPattern.test(window.location.href);
+let previousUrl = window.location.href;
+let tried = 0
+
+// observer
+const observer = new MutationObserver(function () {
+  if (window.location.href !== previousUrl) {
+    previousUrl = window.location.href;
+    runScript();
+  }
+});
+
+// window.addEventListener('load',runScript)
+// observer.observe(document, config);
+
+const config = { subtree: true, childList: true };
 
 
-if (urlMatched) console.log("Hello from campsite watcher")
-else console.log("hello but url didn't match")
+window.addEventListener("load", function () {
+  observer.observe(document.body, config);
+});
+
+
+async function runScript(){
+  const urlPattern = /https:\/\/reservecalifornia.com\/Web\/.+\/.+\/.+/;
+  const urlMatched = urlPattern.test(window.location.href);
+  if (urlMatched) loadColors()
+  else console.log("URL no match: Load")
+  await sleep(5000)
+  runScript()
+}
+
+async function loadColors(){
+    const tableColumn = document.querySelector(`td[data-wg-notranslate="manual"]`);
+    if (!tableColumn) {
+      await sleep(1500)
+      loadColors()
+      return
+    }
+    document.querySelectorAll(`td[data-wg-notranslate="manual"]`).forEach(unit=>{
+      const addNumber = randBetween(0,10)
+      unit.innerHTML = unit.innerHTML.split("<sup>")[0] + `<sup>${addNumber} people</sup>`
+      if (addNumber<3) unit.style.backgroundColor = "#84fa6a"
+      else if (addNumber<6) unit.style.backgroundColor = "#efff42"
+      else unit.style.backgroundColor = "#f66"
+
+      unit.style.paddingLeft="10px"
+      unit.style.borderTop = "2px solid white"
+    })
+}
+
+function randBetween (min,max){
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 
 
 
