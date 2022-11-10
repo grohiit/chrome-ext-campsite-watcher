a3e27aa1e22365bee30b81fb5ccaf1b8c51e7e17
grohiit <81778041+grohiit@users.noreply.github.com>
2022-11-10 17:05:49 -0500

    added real data

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 let previousUrl = window.location.href;
 let tried = 0
+const refreshTimer = 1500
 
 // observer
 const observer = new MutationObserver(function () {
@@ -10,43 +11,61 @@ const observer = new MutationObserver(function () {
   }
 });
 
-// window.addEventListener('load',runScript)
-// observer.observe(document, config);
-
 const config = { subtree: true, childList: true };
 
+window.addEventListener('load',runScript)
+observer.observe(document, config);
 
-window.addEventListener("load", function () {
-  observer.observe(document.body, config);
-});
 
 
 async function runScript(){
   const urlPattern = /https:\/\/reservecalifornia.com\/Web\/.+\/.+\/.+/;
   const urlMatched = urlPattern.test(window.location.href);
   if (urlMatched) loadColors()
-  else console.log("URL no match: Load")
-  await sleep(5000)
+  else console.log("URL no match")
+  await sleep(refreshTimer)
   runScript()
 }
 
 async function loadColors(){
-    const tableColumn = document.querySelector(`td[data-wg-notranslate="manual"]`);
-    if (!tableColumn) {
-      await sleep(1500)
-      loadColors()
-      return
-    }
-    document.querySelectorAll(`td[data-wg-notranslate="manual"]`).forEach(unit=>{
-      const addNumber = randBetween(0,10)
-      unit.innerHTML = unit.innerHTML.split("<sup>")[0] + `<sup>${addNumber} people</sup>`
-      if (addNumber<3) unit.style.backgroundColor = "#84fa6a"
-      else if (addNumber<6) unit.style.backgroundColor = "#efff42"
-      else unit.style.backgroundColor = "#f66"
+  console.log("URL Match")
+  await sleep(refreshTimer)
+  const tableColumn = document.querySelector(`td[data-wg-notranslate="manual"]`);
+  if (!tableColumn) {
+    console.log("No column")
+    loadColors()
+    return
+  }
+  const url = window.location.href
+  const facilityId = url.split("/").pop()
+  const facData = await pingReserveCAServer(facilityId)
 
-      unit.style.paddingLeft="10px"
-      unit.style.borderTop = "2px solid white"
-    })
+  if (!facData.success) {
+    await sleep(refreshTimer)
+    loadColors()
+    return
+  }
+
+  const popups = Object.values(facData.Units).map(unit=>{
+    const {UnitId,Name,ShortName,RecentPopups} = unit
+    return {UnitId,Name,ShortName,RecentPopups}
+  })
+
+
+  
+  document.querySelectorAll(`td[data-wg-notranslate="manual"]`).forEach(unit=>{
+    
+    const popup = popups.filter(campsite=>campsite.Name==unit.textContent.split("-")[0])
+    
+    const addNumber = popup[0]["RecentPopups"]
+    unit.innerHTML = unit.innerHTML.split("<sup>")[0] + `<sup>-${addNumber}</sup>`
+    if (addNumber<3) unit.style.backgroundColor = "#84fa6a"
+    else if (addNumber<6) unit.style.backgroundColor = "#efff42"
+    else unit.style.backgroundColor = "#f66"
+
+    unit.style.paddingLeft="10px"
+    unit.style.borderTop = "2px solid white"
+  })
 }
 
 function randBetween (min,max){
@@ -55,6 +74,55 @@ function randBetween (min,max){
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async function pingReserveCAServer(facilityId){
+    const options = {dateStyle: "short",timeStyle: "long", timeZone: "America/Los_Angeles"}
+    const runTime = new Date().toLocaleString("en-us",options)
+
+    try {
+    const today = new Date().toISOString().split("T")[0]
+    const lastDayStamp = new Date().setDate(new Date().getDate()+180)
+    const lastDay = new Date(lastDayStamp).toISOString().split("T")[0]
+    let i =0
+  
+    const body = `{"IsADA":false,"MinVehicleLength":0,"UnitCategoryId":0,"StartDate":"${today}","WebOnly":true,"UnitTypesGroupIds":[],"SleepingUnitId":0,"EndDate":"${lastDay}","UnitSort":"orderby","InSeasonOnly":true,"FacilityId":"${facilityId}","RestrictADA":false}`
+      
+    
+    
+    const res = await fetch("https://calirdr.usedirect.com/RDR/rdr/search/grid", {
+      "headers": {
+        "accept": "application/json",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json",
+        "sec-ch-ua": '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "cross-site"
+      },
+      referrer: "https://reservecalifornia.com/",
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body ,
+      method: "POST",
+      mode: "cors",
+      credentials: "omit"})
+      
+      const response = await res.json()
+
+      if (!response.Facility.Units) {
+        return {success:false,runTime, message: `${facilityId} failed`}
+      } 
+    
+      const {Facility:{AvailableUnitCount,UnitCount,AvailableSliceCount,SliceCount,Units}} = response
+  
+      return { success: true,runTime, data: {AvailableUnitCount,UnitCount,AvailableSliceCount,SliceCount},Units}
+
+    } catch(e) {
+      console.log("Error: ",e.message)
+      return {success:false,runTime, message: e.message}
+    }
   }
 
 
